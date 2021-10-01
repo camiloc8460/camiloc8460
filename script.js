57e1e857a184318ca62a3bf4590776ddf0206073
camiloc8460 <91750494+camiloc8460@users.noreply.github.com>
2021-10-01 13:06:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Camilo Casanova"; // update the part BETWEEN the "quotes"
+let userName = "camiloc8460@hstat.org"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
